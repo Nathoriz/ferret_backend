7690cbc797ee4e88cecbb1a0f01b653b13c17c99
Mod <natalyortiz.401@gmail.com>
2021-06-18 14:52:38 -0500

    Creación del proyecto

---
+++ src/main/java/com/android/ferret_backend/FerretBackendApplication.java
@@ -0,0 +1,13 @@
+package com.android.ferret_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FerretBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FerretBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/android/ferret_backend/FerretBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.android.ferret_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FerretBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
